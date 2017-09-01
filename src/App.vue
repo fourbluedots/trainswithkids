756af12f1827a7ab173931a937260cc32dc80e15
mrbluedot <mrbluedot@users.noreply.github.com>
2017-08-31 21:43:26 -0400

    Small cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app light>
     <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-list-tile-action>
             <v-icon left>{{item.icon}}</v-icon>
           </v-list-tile-action>
@@ -22,7 +22,7 @@
         </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only" >
-        <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
            <v-icon left>{{item.icon}}</v-icon>
           {{item.title}}
           </v-btn>
